e6a65aaf71f062da7c0dbe0a0e5d4524b4e9a526
UJwal Arak <rootUJ@localhost.localdomain>
2017-11-09 04:45:56 +0530

    added mlab connectivity

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 if (process.env.PORT) {
-    mongoose.connect('mongodb://admin:nimda@ds153015.mlab.com:53015/rest-node',{useMongoClient: true });
+    mongoose.connect(MONGOLAB_URI,{useMongoClient: true });
 }
 else {
     mongoose.connect('mongodb://localhost:27017/TodoApp',{useMongoClient: true });
