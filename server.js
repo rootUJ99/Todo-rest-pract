1c425aaba4f0f33ff44a403b31c6319c1155f4b5
UJwal Arak <rootUJ@localhost.localdomain>
2017-11-15 02:55:43 +0530

    Nothing really

--- server.js
+++ server.js
@@ -6,14 +6,6 @@ const express=require('express');
 const bodyParser=require('body-parser');
 let app=express();
 const port=process.env.PORT||3000;
-
-if (process.env.PORT) {
-    mongoose.connect(MONGOLAB_URI, { useMongoClient: true });
-}
-else {
-    mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient: true });
-}
-
 app.use(bodyParser.json());
 
 //POST req
