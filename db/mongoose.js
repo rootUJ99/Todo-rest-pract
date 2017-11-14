ba42e5f6ca79037794eb70a878b16b0205aa20ac
UJwal Arak <rootUJ@localhost.localdomain>
2017-11-15 04:29:16 +0530

    Added delete code

--- db/mongoose.js
+++ db/mongoose.js
@@ -11,7 +11,7 @@ let db={
     localhost: 'mongodb://localhost:27017/TodoApp',
     mlab: 'mongodb://root:toor@ds153015.mlab.com:53015/rest-node'
 }
-mongoose.connect(db.localhost || db.mlab, { useMongoClient: true });
+mongoose.connect(db.mlab || db.localhost, { useMongoClient: true });
 module.exports={
     mongoose
 }
