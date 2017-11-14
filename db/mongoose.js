dcb8f5658e80b0fb6147d6eaf2c02b172cf8d625
UJwal Arak <rootUJ@localhost.localdomain>
2017-11-15 04:13:06 +0530

    Added delete code

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-if (process.env.PORT) {
-    mongoose.connect('mongodb://root:toor@ds153015.mlab.com:53015/rest-node', { useMongoClient: true });
+// if (process.env.PORT) {
+//     mongoose.connect('mongodb://root:toor@ds153015.mlab.com:53015/rest-node', { useMongoClient: true });
+// }
+// else {
+//     mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient: true });
+// }
+
+let db={
+    localhost: 'mongodb://localhost:27017/TodoApp',
+    mlab: 'mongodb://root:toor@ds153015.mlab.com:53015/rest-node'
 }
-else {
-    mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient: true });
-}
+mongoose.connect(db.localhost || db.mlab, { useMongoClient: true });
 module.exports={
     mongoose
 }
