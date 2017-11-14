f5b86ca622cda411a86063bb0db584064006f5e7
UJwal Arak <rootUJ@localhost.localdomain>
2017-11-15 05:09:19 +0530

    Added delete code

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-// if (process.env.PORT) {
-//     mongoose.connect('mongodb://root:toor@ds153015.mlab.com:53015/rest-node', { useMongoClient: true });
-// }
-// else {
-//     mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient: true });
-// }
-
-let db={
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://root:toor@ds153015.mlab.com:53015/rest-node'
+if (process.env.PORT) {
+    mongoose.connect('mongodb://root:toor@ds153015.mlab.com:53015/rest-node', { useMongoClient: true });
 }
-mongoose.connect(db.mlab || db.localhost, { useMongoClient: true });
+else {
+    mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient: true });
+}
+
+// let db={
+//     localhost: 'mongodb://localhost:27017/TodoApp',
+//     mlab: 'mongodb://root:toor@ds153015.mlab.com:53015/rest-node'
+// }
+// mongoose.connect(db.mlab || db.localhost, { useMongoClient: true });
 module.exports={
     mongoose
 }
